b1f29568b76ce598bc2ea0cba500ddd8c9751d18
milesdavis71 <hevesiistvan71@hotmail.com>
2022-06-12 21:26:41 +0200

    GameControl, Odd, Even komponensek létrehozva.

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+// prettier-ignore
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter
+} from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Output() incrementNumber = new EventEmitter<{ incrementedNumber: number }>();
+  counter = 0;
+  constructor() {}
+  increaseNumber() {
+    setInterval(() => {
+      this.counter += 1;
+    }, 1000);
+    this.incrementNumber.emit({ incrementedNumber: this.counter });
   }
 
+  ngOnInit(): void {}
 }
